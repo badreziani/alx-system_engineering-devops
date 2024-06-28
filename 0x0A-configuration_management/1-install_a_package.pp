e215f619c1ee77dc4f717eac585e1d6711e9a6d5
Badre Ziani <badreziani@gmail.com>
2024-06-28 23:04:58 +0100

    Update task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,17 @@
 # This manifest file installs flask from pip3
 
-package { 'python3-pip':
-  ensure => installed,
+package { 'python3':
+  ensure   => '3.8.10',
+  provider => 'pip3',
 }
 
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3'
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask']
 }
