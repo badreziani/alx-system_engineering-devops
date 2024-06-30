7acfaa77115885396d603da410de3d8c0be83268
Badre Ziani <badreziani@gmail.com>
2024-06-30 06:59:19 -0700

    create task 5

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # connect to a server without typing a password
 
 exec { 'ssh_connect':
-  command => '',
-  path    => i''
+  command => 'echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config; echo Pass"wordAuthentication no" >> /etc/ssh/ssh_config',
+  path    => '/usr/bin'
 }
