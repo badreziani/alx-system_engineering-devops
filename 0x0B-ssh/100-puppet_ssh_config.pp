a1f44b1668b6e0db27b66379ae2168850667d7c9
Badre Ziani <badreziani@gmail.com>
2024-06-30 07:03:39 -0700

    Fix task 5 error

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # connect to a server without typing a password
 
 exec { 'ssh_connect':
-  command => 'echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config; echo Pass"wordAuthentication no" >> /etc/ssh/ssh_config',
+  command => 'echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config; echo "PasswordAuthentication no" >> /etc/ssh/ssh_config',
   path    => '/usr/bin'
 }
