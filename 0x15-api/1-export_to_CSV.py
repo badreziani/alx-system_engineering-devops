b50f7a94b866cb79e7d9335257a003419f13eee6
Badre Ziani <badreziani@gmail.com>
2024-07-29 22:21:31 +0100

    Create task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
-"""Returns information about his/her TODO list progress.
+"""Exports data in the CSV format.
 """
+
+if __name__ == "__main__":
+    import csv
+    import requests
+    import sys
+
+    url = "https://jsonplaceholder.typicode.com/users/{}"
+    user = requests.get(url.format(sys.argv[1])).json()
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos"
+    todos = requests.get(url.format(sys.argv[1])).json()
+    csv_file = "{}.csv".format(sys.argv[1])
+    with open(csv_file, 'w') as csv_file:
+        writer = csv.writer(csv_file, delimiter=",", quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            writer.writerow([sys.argv[1],
+                             user.get("name"),
+                             todo.get('completed'),
+                             todo.get('title')])
