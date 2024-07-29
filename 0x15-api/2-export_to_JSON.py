9b6ae0643f5a068c96dd45ed5d84450bf97ab5c6
Badre Ziani <badreziani@gmail.com>
2024-07-29 22:55:16 +0100

    Update task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
-"""Returns information about his/her TODO list progress.
+"""Exports data in the CSV format.
 """
+
+if __name__ == "__main__":
+    import json
+    import requests
+    import sys
+
+    url = "https://jsonplaceholder.typicode.com/users/{}"
+    user = requests.get(url.format(sys.argv[1])).json()
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos"
+    todos = requests.get(url.format(sys.argv[1])).json()
+    csv_file = "{}.csv".format(sys.argv[1])
+    data = {}
+    uid = sys.argv[1]
+    with open("{}.json".format(uid), "w") as json_file:
+        json.dump({uid: [{
+            'task': todo.get('title'),
+            'completed': todo.get('completed'),
+            'username': user.get('username')
+            } for todo in todos]}, json_file)
