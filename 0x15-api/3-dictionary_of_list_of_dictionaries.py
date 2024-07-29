525c931d62bc5c6a72cbeb684949d23a03034187
Badre Ziani <badreziani@gmail.com>
2024-07-29 23:11:44 +0100

    Create task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
 """Returns information about his/her TODO list progress.
 """
+
+if __name__ == "__main__":
+    import json
+    import requests
+    import sys
+
+    url = "https://jsonplaceholder.typicode.com/users"
+    users = requests.get(url).json()
+    url = "https://jsonplaceholder.typicode.com/todos"
+    todos = requests.get(url).json()
+    with open("todo_all_employees.json", "w") as json_file:
+        data = {user.get("id"): [{'task': todo.get('title'),
+                                  'completed': todo.get('completed'),
+                                  'username': user.get('username')
+                                  } for todo in todos
+                                 if user.get("id") == todo.get('userId')]
+                for user in users}
+        json.dump(data, json_file)
