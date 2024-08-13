731909735bdb4331c629768f9e04c5583fec9545
Badre Ziani <badreziani@gmail.com>
2024-08-13 19:06:44 +0100

    Create task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""0-subs module
+contains the definiton of number_of_subscribers function
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """number_of_subscribers
+    queries the Reddit API and returns the number of subscribers
+    If not a valid subreddit, return 0."""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = requests.get(url)
+    if response.ok:
+        return response.json().get('data').get('subscribers')
+    else:
+        return 0
