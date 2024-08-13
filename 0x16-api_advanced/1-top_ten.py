ed2ad0f4707f693bfbbd70dae89c3c7eb1a5ddb7
Badre Ziani <badreziani@gmail.com>
2024-08-13 16:40:05 -0700

    Create task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""1-top_ten
+contains the definiton of top_ten function
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """top_ten
+    queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit
+    If not a valid subreddit, print None."""
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    response = requests.get(url, allow_redirects=False)
+    if response.ok:
+        hots = response.json().get('data').get('children')
+        for h in hots[:10]:
+            print(h.get('data').get('title'))
+    else:
+        print('None')
