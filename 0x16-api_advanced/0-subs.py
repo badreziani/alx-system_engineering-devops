9d19103ce452884d075bc42f393235e3a28a80e3
Badre Ziani <badreziani@gmail.com>
2024-08-15 17:02:59 +0100

    Update task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,9 +9,12 @@ def number_of_subscribers(subreddit):
     """number_of_subscribers
     queries the Reddit API and returns the number of subscribers
     If not a valid subreddit, return 0."""
+
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = requests.get(url, allow_redirects=False)
-    if response.ok:
-        return response.json().get('data').get('subscribers')
-    else:
+    headers = {'user-agent': 'fakeuser'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
         return 0
+
+    return response.json().get("data").get("subscribers")
