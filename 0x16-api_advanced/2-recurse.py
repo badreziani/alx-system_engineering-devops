97773583eb3f3ec456694650f256dc4266aa3450
Badre Ziani <badreziani@gmail.com>
2024-08-15 17:35:20 +0100

    Create task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+    Uses Reddit API to get all hot posts
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """Get all hot posts"""
+    if after is None:
+        return []
+
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=100&after={}'.format(
+            subreddit, after)
+    headers = {'user-agent': 'fakeuser'}
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            return None
+
+        json = response.json()
+        posts = json.get("data").get("children")
+
+        for post in posts:
+            hot_list.append(post.get("data").get("title"))
+        return hot_list + recurse(subreddit, [], json.get("data").get("after"))
+    except Exception:
+        return None
